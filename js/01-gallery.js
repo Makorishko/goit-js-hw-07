540a9369d0a8b62023400398217d0ed8d7371890
Makorishko <129119228+Makorishko@users.noreply.github.com>
2023-08-09 21:17:23 +0200

    finished

---
+++ js/01-gallery.js
@@ -0,0 +1,43 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+const liItems = galleryItems.map(item => {
+    const li = `<li>
+    <a class="gallery__link" href=${item.original} src=${item.preview}>
+    <img class="js-item" src=${item.preview} ${item.description} data-source="${item.original}" width=330>
+    </a>
+    </li>`;
+    return li;
+}).join('');
+
+galleryContainer.insertAdjacentHTML("beforeend", liItems);
+
+galleryContainer.addEventListener("click", (event) => {
+    event.preventDefault();
+  
+    if (!event.target.classList.contains('js-item')) {
+        return
+    }
+ 
+    const url = event.target.dataset.source;
+    
+
+const instance = basicLightbox.create(`
+    <div class="modal">
+    <img src=${url} width=500 height=450>;
+    
+    </div>
+`)
+
+instance.show()
+});
+
+console.log(basicLightbox);
+    
+   
+
+
+
+
+  
